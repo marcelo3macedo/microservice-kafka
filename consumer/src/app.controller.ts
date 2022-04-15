eb941530824e79a66d208944a0400838033df17c
Marcelo Macedo <marcelo3macedo@gmail.com>
2022-04-15 14:44:52 -0300

    Initial Commit

---
+++ consumer/src/app.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @MessagePattern('storage-ec')
+  getStorage(@Payload() message) {
+    console.log('Message Received', message);
+  }
+}
---
+++ producer/src/app.controller.ts
@@ -0,0 +1,28 @@
+import { Controller, Get } from '@nestjs/common';
+import { Client, ClientKafka, Transport } from '@nestjs/microservices';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Client({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: [ 'localhost:9093' ]
+      },
+      consumer: {
+        groupId: 'ec-consumer'
+      }
+    }
+  })
+  client: ClientKafka
+
+  @Get()
+  sendToStorage() {
+    return this.client.emit('storage-ec', {
+      message: 'remove chicken from storage',
+    });
+  }
+}
